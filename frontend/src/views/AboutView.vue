07f131304c8303ecd79c886d40d3d9db4df62d92
Lucas Germano <lucas.germano@univates.br>
2022-04-25 17:48:47 -0300

    Iniciada a tela de inscrições da pessoa

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,5 +1,15 @@
 <template>
-  <div class="about">
+  <div class="container">
     <h1>This is an about page</h1>
+    <SubscriptionsTable />
   </div>
 </template>
+
+<script>
+import SubscriptionsTable from '@/components/SubscriptionsTable.vue';
+export default {
+  components: {
+    SubscriptionsTable
+  }
+}
+</script>
